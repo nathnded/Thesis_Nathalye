42f537bb0193775a1ca72379701b40923b4975be
rcduarte01 <robertoduarte0612@gmail.com>
2025-11-26 18:26:02 -0600

    Actulización de Ejemplo simulación regresión

--- ejemploSimulacion.R
+++ ejemploSimulacion.R
@@ -1,19 +1,19 @@
-
-
 N<- 1000
-n <- c(30,40,50,60)
+n <- seq(from=10, to=1000, by=10)
 
 
 beta0<- 9
 beta1<- 3
-errbeta0<-c()
-errbeta1<-c()
+MSEbeta0<-c()
+MSEbeta1<-c()
 
 for(j in 1:length(n)){
+  errbeta0<-c()
+  errbeta1<-c()
   for(i in 1:N){
     x<- runif(n[j], min=1, max=30)
     epsilon<- rnorm(n[j],sd=5)
-    y<- beta0+beta1*x+epsilon
+    y<- beta0 + beta1*x + epsilon
     ajuste<- lm(y~x)
     betahat<-as.numeric(coef(ajuste))
     errbeta0[i]<-beta0-betahat[1]
@@ -22,3 +22,8 @@ for(j in 1:length(n)){
   MSEbeta0[j]<-mean(errbeta0^2)
   MSEbeta1[j]<-mean(errbeta1^2)
 }
+
+par(mfrow=c(1,2))
+plot(n,MSEbeta0)
+plot(n,MSEbeta1)
+par(mfrow=c(1,1))
